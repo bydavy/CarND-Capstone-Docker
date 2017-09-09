decadd1bf3b351e3a89ca6a7b6fe091cb28aef73
Davy Leggieri <davy.leggieri@gmail.com>
2017-09-09 14:53:30 -0700

    run.sh will pass parameters to docker run

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-set -e
+set -ex
 
 # Settings from environment
 UDACITY_SOURCE=${UDACITY_SOURCE:-source}
 UDACITY_IMAGE=${UDACITY_IMAGE:-bydavy/carnd-capstone}
 
-docker run --rm -it -p 4567:4567 -v "$UDACITY_SOURCE:/udacity" ${UDACITY_IMAGE}
+docker run --rm -it -p 4567:4567 -v "$UDACITY_SOURCE:/udacity" ${UDACITY_IMAGE} $@
