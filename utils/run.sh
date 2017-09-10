4de17e766b5574436642802eeb72f6aaf75d39c7
Davy Leggieri <davy.leggieri@gmail.com>
2017-09-10 10:27:51 -0700

    If container is laready running, we will attach to it
    This will make it simpler to have multiple shell sessions to a single
    container.

--- utils/run.sh
+++ utils/run.sh
@@ -4,5 +4,11 @@ set -e
 # Settings from environment
 UDACITY_SOURCE=${UDACITY_SOURCE:-`pwd`}
 UDACITY_IMAGE=${UDACITY_IMAGE:-bydavy/carnd-capstone}
+CONTAINER_NAME="udacity_carnd"
 
-docker run --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
+if [ "$(docker ps -a | grep ${CONTAINER_NAME})" ]; then
+  echo "Attaching to running container..."
+  docker exec -it ${CONTAINER_NAME} bash $@
+else
+  docker run --name ${CONTAINER_NAME} --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
+fi
