404efee4ab29931b6ecb6401d0ae5ffd5d3d1eb5
Davy Leggieri <davy.leggieri@gmail.com>
2017-09-09 22:58:31 -0700

    Default source directory is now current directory

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Settings from environment
-UDACITY_SOURCE=${UDACITY_SOURCE:-source}
+UDACITY_SOURCE=${UDACITY_SOURCE:-.}
 UDACITY_IMAGE=${UDACITY_IMAGE:-bydavy/carnd-capstone}
 
 docker run --rm -it -p 4567:4567 -v "${UDACITY_SOURCE}:/udacity" ${UDACITY_IMAGE} $@
